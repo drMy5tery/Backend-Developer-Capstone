3b5444582ffc20f0978d6aeac25ae663e3cb75e4
Jawahar Jeeva <rjeprof@gmail.com>
2023-04-15 00:35:30 +0530

    Django Session Authentication and Token Authentication added

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('restaurant/booking/', include(router.urls))
 ]
 
