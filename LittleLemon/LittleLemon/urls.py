f2b32344b2857a4917182d4bb0abdf3f0ff5a057
Jawahar Jeeva <rjeprof@gmail.com>
2023-04-15 00:57:58 +0530

    Urls.py modified to obtain Token for TokAuth

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from restaurant import views
 
 router=DefaultRouter()
@@ -25,6 +26,7 @@ urlpatterns = [
     path('restaurant/', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token),
     path('restaurant/booking/', include(router.urls))
 ]
 
