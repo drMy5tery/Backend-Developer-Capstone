d6e5e862f98e62d475916be8f5a37009c2d741ca
Jawahar Jeeva <rjeprof@gmail.com>
2023-04-14 10:17:56 +0530

    Serializer for MenuItems is created and djoser is installed & BrowsableAPIRenderer has been added

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Booking(models.Model):
-    Name=models.CharField(max_length=255)
-    No_of_guests=models.IntegerField(max_length=6)
-    BookingDate=models.DateTimeField()
+    name=models.CharField(max_length=255)
+    no_of_guests=models.IntegerField(max_length=6)
+    booking_date=models.DateTimeField()
  
  
 class Menu(models.Model):
-    Title=models.CharField(max_length=255)
-    Price=models.DecimalField(decimal_places=2,max_digits=10)
-    Inventory=models.IntegerField(max_length=5)   
+    title=models.CharField(max_length=255)
+    price=models.DecimalField(decimal_places=2,max_digits=10)
+    inventory=models.IntegerField(max_length=5)   
